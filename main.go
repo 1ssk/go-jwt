0b82e19aee6425b5ff70556d3b47fa65479141c2
ssk <m@82469.ru>
2025-04-17 12:58:42 +0400

    init

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/1ssk/go-jwt/controllers"
+	"github.com/1ssk/go-jwt/initializers"
+	"github.com/1ssk/go-jwt/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func Init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+	initializers.SyncDatabase()
+}
+
+func main() {
+	r := gin.Default()
+
+	Init()
+
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
+
+	r.Run()
+}
